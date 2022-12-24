6bc99a3cada7909c93f693c98add4f31f1c3ecaf
Mecit Can Yazar <102905078+mecitcanyazar@users.noreply.github.com>
2022-12-24 16:22:59 +0300

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from './core/core.module';
+import { NgModule } from '@angular/core';
+import { SharedModule } from './shared/shared.module';
+import { CustomerModule } from './features/customer/customer.module';
+import { StoreModule } from '@ngrx/store';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    HttpClientModule,
+    CustomerModule,
+    StoreModule.forRoot({}, {}),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
